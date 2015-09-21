ba3ce3074ddf696310c3899719d9820893939e4c
kbudd1234 <kevinpmbudd@gmail.com>
2015-09-21 15:26:23 -0500

    feat: added google maps now a google map appears showing places i have lived and worked. Also in addition to this amazing map feature I have completed displaying data from all of the sections of the resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-//var name = "Kevin Budd";
-//var role = "Web Developer";
-
-//var formattedName = HTMLheaderName.replace("%data%", name);
-//var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
-
 var bio = {
 	"name": "Kevin Budd",
 	"role": "Web Developer",
@@ -18,13 +5,46 @@ var bio = {
 		"mobile": "630-373-9596",
 		"email": "kevin@kevinbudd.com",
 		"github": "kevinpmbudd",
-		"website": "www.kevinbudd.com",
 		"location": "Naperville, IL"
 	},
-	"welcomeMessage": "Welcome Home",
-	"skills": ["creative", "natural", "focus", "cooking"],
+	"welcomeMessage": "Welcome! Please take a moment and enjoy learning a little bit about me.",
+	"skills": ["Articulate", "Big Thinker", "Creative", "Detail-Orientated"],
 	"bioPic": "images/me.jpg"
+
+
 };
+
+	bio.display = function () {
+
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+	$(".flex-box").append(formattedMobile);
+	$(".flex-box").append(formattedEmail);
+	$(".flex-box").append(formattedGitHub);
+	$(".flex-box").append(formattedLocation);
+	$("#header").append(formattedBioPic);
+	$("#header").append(formattedWelcomeMsg);
+
+ 	if (bio.skills.length > 0) {
+ 		$("#header").append(HTMLskillsStart);
+ 		for(skill in bio.skills) {
+ 			var formatedskills = HTMLskills.replace("%data%", bio.skills[skill]);
+ 			$("#skills:last").append(formatedskills);
+ 		};
+ 	};
+};
+
+bio.display();
+
 
 var work = {
 	"jobs": [
@@ -33,8 +53,7 @@ var work = {
 		"title": "Web Developer Intern",
 		"location": "Naperville, IL",
 		"dates": "Summer 2015",
-		"description": "Worked on a Cordova cross platform mobile app for Android and iOS. Analyzed and Debugged the app.
-		Created Documentation for work done and future work on project"
+		"description": "Worked on a Cordova cross platform mobile app for Android and iOS. Analyzed and Debugged the app. Created Documentation for work done and future work on project"
 	},
 	{
 		"employer": "Freelance",
@@ -46,6 +65,23 @@ var work = {
 	]
 };
 
+var displayWork = function() {
+
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		var formattedWorkExperience = formattedEmployer + formattedTitle + formattedDates +  formattedLocation + formattedDescription;
+		$(".work-entry:last").append(formattedWorkExperience);
+	}
+
+}
+
+displayWork();
 
 var education = {
 	"schools": [
@@ -53,7 +89,7 @@ var education = {
 		"name": "College of DuPage",
 		"location": "Glen Ellyn, IL",
 		"degree": "Certificate",
-		"focus": ["Java", "ASP.net", "HTML/CSS"],
+		"focus": ["Web & Software Development"],
 		"dates": "2014-2015",
 		"url": "http://cod.edu"
 	},
@@ -65,18 +101,81 @@ var education = {
 		"dates": "2001-2005",
 		"url": "http://depaul.edu"
 	}
-]
-},
+],
 	"onlineCourses": [
 	{
 		"title": "Front End Developer Nanodegree",
 		"school": "Udacity",
-		"dates": "2015-",
+		"dates": "2015",
 		"url": "http://udacity.com"
 
+	},
+	{
+		"title": "Responsive Web Design Fundamentals",
+		"school": "Udacity",
+		"dates": "2015",
+		"url": "http://udacity.com"
+	},
+	{
+		"title": "Responsive Images",
+		"school": "Udacity",
+		"dates": "2015",
+		"url": "http://udacity.com"
+	},
+	{
+		"title": "Javascript Basics",
+		"school": "Udacity",
+		"dates": "2015",
+		"url": "http://udacity.com"
 	}
+
 	]
-}
+};
+
+education.displaySchool = function () {
+
+	for (school in education.schools) {
+		$("#education").append(HTMLschoolStart);
+
+		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		$(".education-entry:last").append(formattedName);
+
+		var formattedDegreee = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		$(".education-entry:last").append(formattedDegreee);
+
+		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		$(".education-entry:last").append(formattedDates);
+
+		for (major in education.schools[school].focus) {
+			var formattedFocus= HTMLschoolMajor.replace("%data%", education.schools[school].focus[major]);
+			$(".education-entry:last").append(formattedFocus);
+		}
+
+		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		$(".education-entry:last").append(formattedLocation);
+	};
+
+	$(".education-entry:last").append(HTMLonlineClasses);
+
+	for (course in education.onlineCourses) {
+
+		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+		$(".education-entry:last").append(formattedTitle);
+
+		var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+		$(".education-entry:last").append(formattedSchool);
+
+		var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+		$(".education-entry:last").append(formattedDates);
+
+		var formattedURL= HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+		$(".education-entry:last").append(formattedURL);
+	};
+};
+
+education.displaySchool();
+
+
 
 var projects = {
 	"projects": [
@@ -84,14 +183,54 @@ var projects = {
 		"title": "Portfolio",
 		"dates": "September 2015",
 		"description": "Online portfolio featuring ongoing work for Front End Developer Nanodegree",
-		"images": ["http://kevinbudd.com#thepark", "http://kevinbudd.com#momandme", "http://kevinbudd.com#bikehelmet"]
+		"images": ["http://kevinbudd.com/img/park.jpg", "http://kevinbudd.com/img/reflection.jpg"]
 
 	}
 	]
+};
+
+projects.display = function () {
+
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (projects.projects[project].images.length > 0) {
+			for (image in projects.projects[project].images) {
+				var formattedprojectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedprojectImage);
+			}
+		}
+	}
 }
 
+projects.display();
 
-$("#main").append(work["position"]);
+
+function inName(name) {
+	name = name.trim().split(" ");
+	console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    name = $("#name").text();
+
+    return name;
+}
+
+$("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
+
+
 
 
 
