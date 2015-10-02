180a5a81e60d9317211de4c11b121e5948074df1
kbudd1234 <kevinpmbudd@gmail.com>
2015-10-02 14:01:11 -0500

    Fix: display function for work was not encapsulated in the work object.Now it is.
    Also performed several changes based on udacity feedback.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,241 +1,236 @@
+/** resumeBuilder contains JSON objects and functions to display them
+  * in order to populate the resume
+  */
+
+// Bio information
 var bio = {
-	"name": "Kevin Budd",
-	"role": "Web Developer",
-	"contacts": {
-		"mobile": "630-373-9596",
-		"email": "kevin@kevinbudd.com",
-		"github": "kevinpmbudd",
-		"location": "Naperville, IL"
-	},
-	"welcomeMessage": "Welcome! Please take a moment and enjoy learning a little bit about me.",
-	"skills": ["HTML/CSS/Javascript", "Java", "SQL", "Still Learning"],
-	"bioPic": "images/me.jpg"
-
-
+    'name': 'Kevin Budd',
+    'role': 'Web Developer',
+    'contacts': {
+        'mobile': '630-373-9596',
+        'email': 'kevin@kevinbudd.com',
+        'github': 'kevinpmbudd',
+        'location': 'Naperville, IL'
+    },
+    'welcomeMessage': 'Welcome! Please take a moment and enjoy learning a little bit about me.',
+    'skills': ['HTML/CSS/Javascript', 'Java', 'SQL', 'Still Learning'],
+    'bioPic': 'images/me.jpg'
 };
 
-	bio.display = function () {
+// function to display bio info
+bio.display = function() {
 
-	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-	var formattedName = HTMLheaderName.replace("%data%", bio.name);
-	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
-	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-	var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
-	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-	var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    'use strict';
+    var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+    var formattedName = HTMLheaderName.replace('%data%', bio.name);
+    var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+    var formattedGitHub = HTMLgithub.replace('%data%', bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+    var formattedBioPic = HTMLbioPic.replace('%data%', bio.bioPic);
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
 
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
-	$(".flex-box").append(formattedMobile);
-	$(".flex-box").append(formattedEmail);
-	$(".flex-box").append(formattedGitHub);
-	$(".flex-box").append(formattedLocation);
-	$("#header").append(formattedBioPic);
-	$("#header").append(formattedWelcomeMsg);
+    $('#header').prepend(formattedRole);
+    $('#header').prepend(formattedName);
+    $('.flex-box').append(formattedMobile);
+    $('.flex-box').append(formattedEmail);
+    $('.flex-box').append(formattedGitHub);
+    $('.flex-box').append(formattedLocation);
+    $('#header').append(formattedBioPic);
+    $('#header').append(formattedWelcomeMsg);
 
- 	if (bio.skills.length > 0) {
- 		$("#header").append(HTMLskillsStart);
- 		for(skill in bio.skills) {
- 			var formatedskills = HTMLskills.replace("%data%", bio.skills[skill]);
- 			$("#skills:last").append(formatedskills);
- 		};
- 	};
+    if (bio.skills.length) {
+        $('#header').append(HTMLskillsStart);
+        bio.skills.forEach(function(skill) {
+            var formatedskills = HTMLskills.replace('%data%', skill);
+            $('#skills:last').append(formatedskills);
+        });
+    }
 };
 
 bio.display();
 
-
+// work information
 var work = {
-	"jobs": [
-	{
-		"employer": "Trace-it Systems",
-		"title": "Web Developer Intern",
-		"location": "Naperville, IL",
-		"dates": "Summer 2015",
-		"description": "Worked on a Cordova cross platform mobile app for Android and iOS. Analyzed and Debugged the app. Created Documentation for work done and future work on project"
-	},
-	{
-		"employer": "Freelance",
-		"title": "Yoga Teacher",
-		"location": "Naperville, IL",
-		"dates": "2010-2014",
-		"description": "Taught weekly classes, workshops and teacher trainings. Maintained website for promoting my teaching."
-	}
-	]
+    'jobs': [{
+        'employer': 'Trace-it Systems',
+        'title': 'Web Developer Intern',
+        'location': 'Naperville, IL',
+        'dates': 'Summer 2015',
+        'description': 'Worked on a Cordova cross platform mobile app for Android and iOS. Analyzed and Debugged the app. Created Documentation for work done and future work on project'
+    }, {
+        'employer': 'Freelance',
+        'title': 'Yoga Teacher',
+        'location': 'Naperville, IL',
+        'dates': '2010-2014',
+        'description': 'Taught weekly classes, workshops and teacher trainings. Maintained website for promoting my teaching.'
+    }]
 };
 
-var displayWork = function() {
+// function to display work info
+work.display = function() {
 
-	for (job in work.jobs) {
-		$("#workExperience").append(HTMLworkStart);
+    'use strict';
+    work.jobs.forEach(function(job) {
+        $('#workExperience').append(HTMLworkStart);
 
-		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-		var formattedWorkExperience = formattedEmployer + formattedTitle + formattedDates +  formattedLocation + formattedDescription;
-		$(".work-entry:last").append(formattedWorkExperience);
-	}
+        var formattedEmployer = HTMLworkEmployer.replace('%data%', job.employer);
+        var formattedTitle = HTMLworkTitle.replace('%data%', job.title);
+        var formattedDates = HTMLworkDates.replace('%data%', job.dates);
+        var formattedLocation = HTMLworkLocation.replace('%data%', job.location);
+        var formattedDescription = HTMLworkDescription.replace('%data%', job.description);
+        var formattedWorkExperience = formattedEmployer + formattedTitle + formattedDates + formattedLocation + formattedDescription;
+        $('.work-entry:last').append(formattedWorkExperience);
+    });
 
-}
+};
 
-displayWork();
+work.display();
 
+// education info
 var education = {
-	"schools": [
-	{
-		"name": "College of DuPage",
-		"location": "Glen Ellyn, IL",
-		"degree": "Certificate",
-		"focus": ["Web & Software Development"],
-		"dates": "2014-2015",
-		"url": "http://cod.edu"
-	},
-	{
-		"name": "DePaul University",
-		"location": "Chicago, IL",
-		"degree": "BS",
-		"focus": ["Human-Computer Interaction"],
-		"dates": "2001-2005",
-		"url": "http://depaul.edu"
-	}
-],
-	"onlineCourses": [
-	{
-		"title": "Front End Developer Nanodegree",
-		"school": "Udacity",
-		"dates": "2015",
-		"url": "http://udacity.com"
+    'schools': [{
+        'name': 'College of DuPage',
+        'location': 'Glen Ellyn, IL',
+        'degree': 'Certificate',
+        'focus': ['Web & Software Development'],
+        'dates': '2014-2015',
+        'url': 'http://cod.edu'
+    }, {
+        'name': 'DePaul University',
+        'location': 'Chicago, IL',
+        'degree': 'BS',
+        'focus': ['Human-Computer Interaction'],
+        'dates': '2001-2005',
+        'url': 'http://depaul.edu'
+    }],
+    'onlineCourses': [{
+            'title': 'Front End Developer Nanodegree',
+            'school': 'Udacity',
+            'dates': '2015',
+            'url': 'http://udacity.com'
 
-	},
-	{
-		"title": "Responsive Web Design Fundamentals",
-		"school": "Udacity",
-		"dates": "2015",
-		"url": "http://udacity.com"
-	},
-	{
-		"title": "Responsive Images",
-		"school": "Udacity",
-		"dates": "2015",
-		"url": "http://udacity.com"
-	},
-	{
-		"title": "Javascript Basics",
-		"school": "Udacity",
-		"dates": "2015",
-		"url": "http://udacity.com"
-	}
+        }, {
+            'title': 'Responsive Web Design Fundamentals',
+            'school': 'Udacity',
+            'dates': '2015',
+            'url': 'http://udacity.com'
+        }, {
+            'title': 'Responsive Images',
+            'school': 'Udacity',
+            'dates': '2015',
+            'url': 'http://udacity.com'
+        }, {
+            'title': 'Javascript Basics',
+            'school': 'Udacity',
+            'dates': '2015',
+            'url': 'http://udacity.com'
+        }
 
-	]
+    ]
 };
 
-education.displaySchool = function () {
+// function to display education info
+education.displaySchool = function() {
 
-	for (school in education.schools) {
-		$("#education").append(HTMLschoolStart);
+    'use strict';
+    education.schools.forEach(function(school) {
+        $('#education').append(HTMLschoolStart);
 
-		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-		$(".education-entry:last").append(formattedName);
+        var formattedName = HTMLschoolName.replace('%data%', school.name);
+        $('.education-entry:last').append(formattedName);
 
-		var formattedDegreee = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-		$(".education-entry:last").append(formattedDegreee);
+        var formattedDegreee = HTMLschoolDegree.replace('%data%', school.degree);
+        $('.education-entry:last').append(formattedDegreee);
 
-		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-		$(".education-entry:last").append(formattedDates);
+        var formattedDates = HTMLschoolDates.replace('%data%', school.dates);
+        $('.education-entry:last').append(formattedDates);
 
-		for (major in education.schools[school].focus) {
-			var formattedFocus= HTMLschoolMajor.replace("%data%", education.schools[school].focus[major]);
-			$(".education-entry:last").append(formattedFocus);
-		}
+        school.focus.forEach(function(major) {
+            var formattedFocus = HTMLschoolMajor.replace('%data%', major);
+            $('.education-entry:last').append(formattedFocus);
+        });
 
-		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-		$(".education-entry:last").append(formattedLocation);
-	};
+        var formattedLocation = HTMLschoolLocation.replace('%data%', school.location);
+        $('.education-entry:last').append(formattedLocation);
+    });
 
-	$(".education-entry:last").append(HTMLonlineClasses);
+    $('.education-entry:last').append(HTMLonlineClasses);
 
-	for (course in education.onlineCourses) {
+    education.onlineCourses.forEach(function(course) {
 
-		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
-		$(".education-entry:last").append(formattedTitle);
+        var formattedTitle = HTMLonlineTitle.replace('%data%', course.title);
+        $('.education-entry:last').append(formattedTitle);
 
-		var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
-		$(".education-entry:last").append(formattedSchool);
+        var formattedSchool = HTMLonlineSchool.replace('%data%', course.school);
+        $('.education-entry:last').append(formattedSchool);
 
-		var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
-		$(".education-entry:last").append(formattedDates);
+        var formattedDates = HTMLonlineDates.replace('%data%', course.dates);
+        $('.education-entry:last').append(formattedDates);
 
-		var formattedURL= HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
-		$(".education-entry:last").append(formattedURL);
-	};
+        var formattedURL = HTMLonlineURL.replace('%data%', course.url);
+        $('.education-entry:last').append(formattedURL);
+    });
 };
 
 education.displaySchool();
 
-
-
+// project info
 var projects = {
-	"projects": [
-	{
-		"title": "http://www.kevinbudd.com\"\>Portfolio",
-		"dates": "September 2015",
-		"description": "Online portfolio featuring ongoing work for Front End Developer Nanodegree",
-		"images": ["http://kevinbudd.com/img/park.jpg", "http://kevinbudd.com/img/reflection.jpg"]
-	},
-	{
-		"title": "http://students.thatlink.com/Student11//Project5.aspx\"\>Coffee Cupping Database",
-		"dates": "May 2015",
-		"description": "SQL Database storing results of coffee cupping with visual graphs represeting results",
-		"images": ["images/coffee_front.jpg", "images/coffee_graph.jpg"]
-	}
-	]
+    'projects': [{
+        'title': 'http://www.kevinbudd.com\'\>Portfolio',
+        'dates': 'September 2015',
+        'description': 'Online portfolio featuring ongoing work for Front End Developer Nanodegree',
+        'images': ['http://kevinbudd.com/img/park.jpg', 'http://kevinbudd.com/img/reflection.jpg']
+    }, {
+        'title': 'http://students.thatlink.com/Student11//Project5.aspx\'\>Coffee Cupping Database',
+        'dates': 'May 2015',
+        'description': 'SQL Database storing results of coffee cupping with visual graphs represeting results',
+        'images': ['images/coffee_front.jpg', 'images/coffee_graph.jpg']
+    }]
 };
 
-projects.display = function () {
+// function to display project info
+projects.display = function() {
 
-	for (project in projects.projects) {
-		$("#projects").append(HTMLprojectStart);
+    'use strict';
+    projects.projects.forEach(function(project) {
+        $('#projects').append(HTMLprojectStart);
 
-		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-		$(".project-entry:last").append(formattedTitle);
+        var formattedTitle = HTMLprojectTitle.replace('%data%', project.title);
+        $('.project-entry:last').append(formattedTitle);
 
-		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-		$(".project-entry:last").append(formattedDates);
+        var formattedDates = HTMLprojectDates.replace('%data%', project.dates);
+        $('.project-entry:last').append(formattedDates);
 
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-		$(".project-entry:last").append(formattedDescription);
+        var formattedDescription = HTMLprojectDescription.replace('%data%', project.description);
+        $('.project-entry:last').append(formattedDescription);
 
-		if (projects.projects[project].images.length > 0) {
-			for (image in projects.projects[project].images) {
-				var formattedprojectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-				$(".project-entry:last").append(formattedprojectImage);
-			}
-		}
-	}
-}
+        if (project.images.length) {
+            project.images.forEach(function(image) {
+                var formattedprojectImage = HTMLprojectImage.replace('%data%', image);
+                $('.project-entry:last').append(formattedprojectImage);
+            });
+        }
+    });
+};
 
 projects.display();
 
-
+//function to uppercase last name and capatalize first name
 function inName(name) {
-	name = name.trim().split(" ");
-	console.log(name);
+    name = name.trim().split(' ');
+    console.log(name);
     name[1] = name[1].toUpperCase();
-    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
 
-    name = $("#name").text();
+    name = $('#name').text();
 
     return name;
 }
 
-$("#main").append(internationalizeButton);
+// display internationalize button
+$('#main').append(internationalizeButton);
 
-$("#mapDiv").append(googleMap);
-
-
-
-
-
+// display good map
+$('#mapDiv').append(googleMap);
